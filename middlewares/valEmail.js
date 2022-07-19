9c38cfa1c1c96956c3966c36cff2e3e0342e9282
raphabarzotto <raphabarzotto@gmail.com>
2022-07-19 17:42:56 -0300

    feat: add Email validation logic

---
+++ middlewares/valEmail.js
@@ -0,0 +1,12 @@
+const valEmail = (req, res, next) => {
+  const { email } = req.body;
+// RegExr
+  const re = /\S+@\S+\.com/;
+  if (!email) 
+    return res.status(400).json({ message: 'O campo "email" é obrigatório' });
+  if (!re.test(email)) 
+    return res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
+  next();
+};
+
+module.exports = valEmail; 
