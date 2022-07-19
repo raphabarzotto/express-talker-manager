4d124279f0dd8a4eca2d6c7fb1ba06d618ce4368
raphabarzotto <raphabarzotto@gmail.com>
2022-07-19 17:50:24 -0300

    fix: lint

--- middlewares/valEmail.js
+++ middlewares/valEmail.js
@@ -5,7 +5,7 @@ const valEmail = (req, res, next) => {
   if (!email) {
     return res.status(400).json({ message: 'O campo "email" é obrigatório' });
   }
-  if (!re.test(email)){
+  if (!re.test(email)) {
     return res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
   }
   next();
