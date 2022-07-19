a5c40d8f0ecc9dab76cd1ff90f641fadf7d2c46f
raphabarzotto <raphabarzotto@gmail.com>
2022-07-19 17:40:03 -0300

    feat: add Email and Password validations on Login Router

--- routers/routerLogin.js
+++ routers/routerLogin.js
@@ -1,10 +1,9 @@
 const router = require('express').Router();
 const generateToken = require('../helpers/generateToken');
+const valEmail = require('../middlewares/valEmail');
+const valPassword = require('../middlewares/valPassword');
 
-router.post('/', (req, res) => {
-  const { email, password } = req.body;
-  console.log({ email, password });
-  // console.log para passar do req 3
+router.post('/', [valEmail, valPassword], (_req, res) => {
   const token = generateToken();
 
   return res.status(200).json({ token });
