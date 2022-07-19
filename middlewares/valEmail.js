844791c21009bcf52fb7f18a3a9b569afc7dcd04
raphabarzotto <raphabarzotto@gmail.com>
2022-07-19 17:48:35 -0300

    fix: fix ifs

--- middlewares/valEmail.js
+++ middlewares/valEmail.js
@@ -2,10 +2,12 @@ const valEmail = (req, res, next) => {
   const { email } = req.body;
 // RegExr
   const re = /\S+@\S+\.com/;
-  if (!email) 
+  if (!email) {
     return res.status(400).json({ message: 'O campo "email" é obrigatório' });
-  if (!re.test(email)) 
+  }
+  if (!re.test(email)){
     return res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
+  }
   next();
 };
 
--- middlewares/valPassword.js
+++ middlewares/valPassword.js
@@ -1,9 +1,11 @@
 const validPassword = (req, res, next) => {
   const { password } = req.body;
-  if (!password) 
+  if (!password) {
     return res.status(400).json({ message: 'O campo "password" é obrigatório' });
-  if (password.length < 6) 
+  }
+  if (password.length < 6) {
     return res.status(400).json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
+  }
   next();
 };
 
