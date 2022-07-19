7f8fea95521676b6581637ceef952bb853fcbbde
raphabarzotto <raphabarzotto@gmail.com>
2022-07-19 18:07:42 -0300

    feat: add token authorization to Login Router request

--- routers/routerLogin.js
+++ routers/routerLogin.js
@@ -3,8 +3,9 @@ const generateToken = require('../helpers/generateToken');
 const valEmail = require('../middlewares/valEmail');
 const valPassword = require('../middlewares/valPassword');
 
-router.post('/', [valEmail, valPassword], (_req, res) => {
+router.post('/', [valEmail, valPassword], (req, res) => {
   const token = generateToken();
+  req.authorization = token;
 
   return res.status(200).json({ token });
 });
